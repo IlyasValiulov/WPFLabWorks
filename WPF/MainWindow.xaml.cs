b3f98cc173a7751b98a65b93251d7993becdabe5
IlyasValiulov <148232695+IlyasValiulov@users.noreply.github.com>
2024-10-04 14:33:22 +0400

    Лабарторная 4 Измененная

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -16,9 +16,43 @@ namespace WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ICommand ShowMessageBox { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            ShowMessageBox = new RelayCommand(LoginShowBox);
+            DataContext = this;
+        }
+        private void LoginShowBox(object parameter)
+        {
+            MessageBox.Show("Вход");
+        }
+    }
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
         }
     }
 }
